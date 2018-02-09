af47fb00eedaf5342796d22a38c2974c3972d98f
Alexander Urizar <urizaraj@gmail.com>
2018-02-09 16:01:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(names)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_plane(elements)
+  result = elements.map do |element|
+    element[0].upcase + element[1..-1] + '!'
+  end
 end
 
 def long_planeteer_calls# code an argument here
