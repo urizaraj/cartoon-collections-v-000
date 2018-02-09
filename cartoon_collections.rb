4b2f1258d8954f7066ff5552047055ae23bf88c3
Alexander Urizar <urizaraj@gmail.com>
2018-02-09 15:59:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.sort!
+  names.each do |name|
+    puts name
+  end
 end
 
 def summon_captain_planet# code an argument here
